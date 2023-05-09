5461f4643d360f338c4777bd70ce69abfe7a5e97
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2023-05-09 21:23:07 +0530

    cors

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express();
 const mongoose = require("mongoose");
 
 app.use((req, res, next) => {
-  res.header({ "Access-Control-Allow-Origin": "*" });
+  res.header({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "PUT, POST, GET, DELETE",
+  });
   next();
 });
 
